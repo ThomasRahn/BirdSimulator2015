056311dfa341c66a6f045c005068fb5eadc5a3b2
Tim Smith <tesmith@gmail.com>
2015-01-17 17:07:09 -0500

    Created a dummy flight scene, and basic controls for gamepad. Slightly improved folder structure with added scripts folder. Flight controls as follows: -Left joystick (sometimeis dpad, depending on model you use) - turn, ascend/descend -left/right + L1 or R1 - hard turn -up + L1+R1 - dive bomb

---
+++ BirdSimulator2015/Assets/Scripts/dummyCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dummyCamera : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//always stare at the bird
+		this.transform.LookAt (player.transform.position);
+	}
+}
