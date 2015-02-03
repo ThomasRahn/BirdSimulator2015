7f705664398e85e04f14d83388e95abc8fc9bb9b
David Siekut <davidsiekut@gmail.com>
2015-02-03 03:16:56 -0500

    Manual merge networking stuff.

---
+++ BirdSimulator2015/Assets/Code/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+	public GameObject Owner;
+	public GameObject Proxy;
+	
+	void uLink_OnServerInitialized()
+	{
+		Debug.Log("OnServerInitialized()");
+		uLink.Network.Instantiate(uLink.Network.player, Proxy, Owner, Owner, Vector3.zero, Quaternion.identity, 0);
+	}
+
+	void uLink_OnConnectedToServer()
+	{
+		Debug.Log("OnConnectedToServer()");
+	}
+
+	void uLink_OnPlayerConnected(uLink.NetworkPlayer player)
+	{
+		Debug.Log("OnPlayerConnected() " + player.ipAddress + ":" + player.port);
+		uLink.Network.Instantiate(player, Proxy, Owner, Proxy, Vector3.zero, Quaternion.identity, 0);
+	}
+
+	void uLink_OnPlayerDisconnected(uLink.NetworkPlayer player)
+	{
+		uLink.Network.DestroyPlayerObjects(player);
+		uLink.Network.RemoveRPCs(player);
+	}
+
+	void uLink_OnFailedToConnect(uLink.NetworkConnectionError error)
+	{
+		Debug.LogError("OnFailedToConnect() " + error);
+	}
+}
