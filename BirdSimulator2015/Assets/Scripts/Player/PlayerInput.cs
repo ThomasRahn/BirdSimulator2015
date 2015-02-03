b45afec43c9f98831c1bdddbeac9be86ee72df55
David Siekut <davidsiekut@gmail.com>
2015-02-03 01:27:38 -0500

    Basic networking.

---
+++ BirdSimulator2015/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		Vector3 pos = this.transform.position;
+		pos.x += Input.GetAxis("Horizontal");
+		pos.z += Input.GetAxis("Vertical");
+
+		this.transform.position = pos;
+	}
+}
---
+++ BirdSimulator2015/Assets/Scripts/Test/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+	public GameObject Owner;
+	public GameObject Proxy;
+
+	void uLink_OnPlayerConnected(uLink.NetworkPlayer player)
+	{
+		uLink.Network.Instantiate(player, Proxy, Owner, Proxy, Vector3.zero, Quaternion.identity, 0);
+	}
+
+	void uLink_OnConnectedToServer() 
+	{
+	} 
+
+	void uLink_OnServerInitialized()
+	{
+		uLink.Network.Instantiate(uLink.Network.player, Proxy, Owner, Owner, Vector3.zero, Quaternion.identity, 0);
+	}
+}
