761f691e74fd3f49bc7f33c8e9b8a831fe3c701a
Tim Smith <tesmith@gmail.com>
2015-03-14 15:40:29 -0400

    added texture to world particle, hopefully fixed broken prefab

--- BirdSimulator2015/Assets/Code/Scripts/Player/PlayerParticles.cs
+++ BirdSimulator2015/Assets/Code/Scripts/Player/PlayerParticles.cs
@@ -4,15 +4,14 @@ using System.Collections;
 public class PlayerParticles : MonoBehaviour {
 
 	public GameObject worldParticles;
-	public float particlesForwardOffset = 30f;
-	// Use this for initialization
+
 	void Start () {
-		GameObject temp = Instantiate (worldParticles, this.transform.position+this.transform.forward*particlesForwardOffset, Quaternion.identity) as GameObject;
+		GameObject temp = Instantiate (worldParticles, this.transform.position, Quaternion.identity) as GameObject;
 		temp.transform.parent = this.transform;
+		temp.transform.LookAt (this.transform.forward);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 }
