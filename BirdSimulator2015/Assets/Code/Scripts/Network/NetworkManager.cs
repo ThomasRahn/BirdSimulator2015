ed9d4667b0cda331a0a2f6075b759e71f711fdf6
David Siekut <davidsiekut@gmail.com>
2015-03-13 23:09:19 -0400

    Falling rocks.

--- BirdSimulator2015/Assets/Code/Scripts/Network/NetworkManager.cs
+++ BirdSimulator2015/Assets/Code/Scripts/Network/NetworkManager.cs
@@ -5,19 +5,22 @@ public class NetworkManager : MonoBehaviour
 	public GameObject Owner;
 	public GameObject Proxy;
 
-    Vector3 PLAYER_ROTATION = new Vector3(0f, 163f, 0f); 
+    Vector3 PLAYER_ROTATION = new Vector3(0f, 90f, 0f); 
 
     // ravenhome
     //Vector3 PLAYER_SPAWN = new Vector3(-2238f, 2531f, 254f);
 
+    // outside entrance
+    //Vector3 PLAYER_SPAWN = new Vector3(-580f, 2236f, -523f);
+
     // entrance
-    Vector3 PLAYER_SPAWN = new Vector3(300f, 2050, -495f);
+    //Vector3 PLAYER_SPAWN = new Vector3(300f, 2050, -495f);
 
     // pillar
     //Vector3 PLAYER_SPAWN = new Vector3(556f, 1965f, -502f);
 
     // lower
-    //Vector3 PLAYER_SPAWN = new Vector3(900f, 1365f, -502f);
+    Vector3 PLAYER_SPAWN = new Vector3(900f, 1365f, -502f);
 
 	void uLink_OnServerInitialized()
 	{
---
+++ BirdSimulator2015/Assets/Code/Scripts/Trap/FallingRocks.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FallingRocks : MonoBehaviour
+{
+    public float Width = 0f;
+
+    private List<Object> rocks = new List<Object>();
+    private float timer = 1f;
+
+	void Start()
+    {
+        Object r1 = Resources.Load("Prefabs/Crypt/Environment/pf_Env_FallingRock1");
+        Object r2 = Resources.Load("Prefabs/Crypt/Environment/pf_Env_FallingRock2");
+        Object r3 = Resources.Load("Prefabs/Crypt/Environment/pf_Env_FallingRock3");
+        rocks.Add(r1);
+        rocks.Add(r2);
+        rocks.Add(r3);
+	}
+	
+	void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            timer = Random.Range(1f, 5f);
+            int r = Random.Range(0, rocks.Count);
+            GameObject g = GameObject.Instantiate(rocks[0]) as GameObject;
+            g.transform.rotation = Random.rotation;
+            Vector3 v = this.transform.position;
+            v.x += Random.Range(-Width, Width);
+            g.transform.position = v;
+        }
+	}
+}
