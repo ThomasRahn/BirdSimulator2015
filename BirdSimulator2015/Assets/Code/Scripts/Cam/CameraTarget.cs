c918d59ca0d873cc41a6c5a53bc9b9fc022be2fb
ThomasRahn <thomas@rahn.ca>
2015-03-19 11:40:26 -0400

    Speed up camera while turning.

--- BirdSimulator2015/Assets/Code/Scripts/Cam/CameraTarget.cs
+++ BirdSimulator2015/Assets/Code/Scripts/Cam/CameraTarget.cs
@@ -10,6 +10,7 @@ namespace BirdSimulator2015.Code.Scripts.Cam
 	{
 		private GameObject bird;
 		private bool followRotation;
+		private float turn_speed = 5.0f;
 
 		private void Awake()
 		{
@@ -33,6 +34,19 @@ namespace BirdSimulator2015.Code.Scripts.Cam
 				}
 
 				transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(targetRotation), 2*Time.deltaTime);
+
+				//Want to turn faster then you ascend/descend, but still slow down after turning.
+				Vector3 turn_vector = new Vector3(transform.eulerAngles.x, targetRotation.y, transform.eulerAngles.z);
+
+				if((turn_vector.y - transform.eulerAngles.y) < 20)
+				{
+					transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(turn_vector), 2.0f * Time.deltaTime);
+				}
+				else
+				{
+					transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(turn_vector), turn_speed * Time.deltaTime);
+				}
+
 			}
 		}
 
