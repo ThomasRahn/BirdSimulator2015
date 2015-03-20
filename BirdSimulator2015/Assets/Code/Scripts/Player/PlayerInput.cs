424d424ce4e6f8566f3f6e57eeb47cfb2cc94d7f
David Siekut <davidsiekut@gmail.com>
2015-03-19 23:57:20 -0400

    Normalized all controls to xbox360, removed f310 controls.

--- BirdSimulator2015/Assets/Code/Scripts/Player/PlayerInput.cs
+++ BirdSimulator2015/Assets/Code/Scripts/Player/PlayerInput.cs
@@ -58,71 +58,39 @@ public class PlayerInput : MonoBehaviour
         if (Locked)
             return;
 
-        if (GameController.Gamepad.GetGamepadType() == GamepadSetup.GamepadType.LOGITECHF310)
+        if (GameController.Gamepad.GetGamepadType() == GamepadSetup.GamepadType.XBOX360)
         {
             cameraMultiplier = 5f;
 
-            JoystickAxisX = Input.GetAxisRaw("JoystickAxisX");
-            JoystickAxisY = Input.GetAxisRaw("JoystickAxisY");
-            JoystickAxis4 = Input.GetAxisRaw("JoystickAxis3");
-            JoystickAxis5 = Input.GetAxisRaw("JoystickAxis4");
+            JoystickAxisX = Input.GetAxisRaw("JoystickAxisX"); // left thumbstick horizontal
+            JoystickAxisY = Input.GetAxisRaw("JoystickAxisY"); // left thumbstick vertical
 
-            JoystickButton0 = Input.GetButton("JoystickButton0");
-            JoystickButton1 = Input.GetButton("JoystickButton1");
-            JoystickButton2 = Input.GetButton("JoystickButton2");
-            JoystickButton3 = Input.GetButton("JoystickButton3");
-            JoystickButton4 = Input.GetButton("JoystickButton4");
-            JoystickButton5 = Input.GetButton("JoystickButton5");
-            JoystickButton6 = Input.GetButton("JoystickButton6");
-            JoystickButton7 = Input.GetButton("JoystickButton7");
-            JoystickButton8 = Input.GetButton("JoystickButton8");
-            JoystickButton9 = Input.GetButton("JoystickButton9");
-            JoystickButton10 = Input.GetButton("JoystickButton10");
-            JoystickButton11 = Input.GetButton("JoystickButton11");
-            JoystickButton12 = Input.GetButton("JoystickButton12");
-
+#if UNITY_EDITOR_WIN
+            DoXBOXWin();
+#endif
+#if UNITY_STANDALONE_WIN
+            DoXBOXWin();
+#endif
+#if UNITY_EDITOR_OSX
+            DoXBOXMac();
+#endif
+#if UNITY_STANDALONE_OSX
+            DoXBOXMac();
+#endif
         }
         else if (GameController.Gamepad.GetGamepadType() == GamepadSetup.GamepadType.KEYBOARD)
         {
             JoystickAxisX = Input.GetAxisRaw("KeyboardAxisX");
             JoystickAxisY = Input.GetAxisRaw("KeyboardAxisY");
-			JoystickAxis4 = Input.GetAxis("Mouse X");
-			JoystickAxis5 = Input.GetAxis("Mouse Y");
+            JoystickAxis4 = Input.GetAxis("Mouse X");
+            JoystickAxis5 = Input.GetAxis("Mouse Y");
 
-            JoystickButton0 = Input.GetButton("KeyboardE");
-            JoystickButton1 = Input.GetButton("KeyboardX");
-            JoystickButton2 = Input.GetButton("KeyboardF");
+            JoystickButton0 = Input.GetButton("KeyboardX");
+            JoystickButton1 = Input.GetButton("KeyboardF");
+            JoystickButton2 = Input.GetButton("KeyboardE");
             JoystickButton3 = Input.GetButton("KeyboardQ");
-			JoystickButton5 = Input.GetButton("KeyboardShift");
+            JoystickButton5 = Input.GetButton("KeyboardShift");
         }
-        else if (GameController.Gamepad.GetGamepadType() == GamepadSetup.GamepadType.XBOX360)
-        {
-            cameraMultiplier = 5f;
-
-            JoystickAxisX = Input.GetAxisRaw("JoystickAxisX");
-            JoystickAxisY = Input.GetAxisRaw("JoystickAxisY");
-
-#if UNITY_EDITOR_WIN
-            JoystickAxis4 = Input.GetAxisRaw("JoystickAxis4");
-            JoystickAxis5 = Input.GetAxisRaw("JoystickAxis5");
-            JoystickButton0 = Input.GetButton("JoystickButton0");
-            JoystickButton1 = Input.GetButton("JoystickButton1");
-            JoystickButton2 = Input.GetButton("JoystickButton2");
-            JoystickButton3 = Input.GetButton("JoystickButton3");
-#endif
-#if UNITY_EDITOR_OSX
-            JoystickAxis4 = Input.GetAxisRaw("JoystickAxis3");
-            JoystickAxis5 = Input.GetAxisRaw("JoystickAxis4");
-            JoystickButton0 = Input.GetButton("JoystickButton18");
-            JoystickButton1 = Input.GetButton("JoystickButton16");
-            JoystickButton2 = Input.GetButton("JoystickButton17");
-            JoystickButton3 = Input.GetButton("JoystickButton19");
-            JoystickButton5 = Input.GetButton("JoystickButton14");
-#endif
-	}
-		
-	if (Locked)
-            return;
 
         animator.SetFloat("Horizontal", JoystickAxisX);
         animator.SetFloat("Vertical", JoystickAxisY * invertY);
@@ -137,13 +105,13 @@ public class PlayerInput : MonoBehaviour
             Cameras.Input(JoystickAxis4 * cameraMultiplier, JoystickAxis5 * cameraMultiplier);
 		}
 
-        if (JoystickButton0)
+        if (JoystickButton2)
         {
             this.GetComponent<PlayerSync>().SendTrigger("t_DashForward");
             animator.SetTrigger("t_DashForward");
         }
 
-        if (JoystickButton1)
+        if (JoystickButton0)
         {
             // knees weak moms spaghetti
             if (this.GetComponent<PlayerState>().LandTarget != null)
@@ -160,7 +128,7 @@ public class PlayerInput : MonoBehaviour
             }
         }
 
-        if (JoystickButton2)
+        if (JoystickButton1)
         {
             this.GetComponent<PlayerSync>().SendBool("b_Decelerating", true);
 			animator.SetBool("b_Decelerating", true);
@@ -209,5 +177,44 @@ public class PlayerInput : MonoBehaviour
     public float GetAxisVertical()
     {
         return JoystickAxisY;
+    }
+
+    private void DoXBOXWin()
+    {
+        JoystickAxis3 = Input.GetAxisRaw("JoystickAxis3"); // right and left trigger
+        JoystickAxis4 = Input.GetAxisRaw("JoystickAxis4"); // right thumbstick horizontal
+        JoystickAxis5 = Input.GetAxisRaw("JoystickAxis5"); // right thumbstick vertical
+
+        JoystickButton0 = Input.GetButton("JoystickButton0"); // bottom button
+        JoystickButton1 = Input.GetButton("JoystickButton1"); // right button
+        JoystickButton2 = Input.GetButton("JoystickButton2"); // left button
+        JoystickButton3 = Input.GetButton("JoystickButton3"); // top button
+
+        JoystickButton4 = Input.GetButton("JoystickButton4"); // left bumper
+        JoystickButton5 = Input.GetButton("JoystickButton5"); // right bumper
+        JoystickButton6 = Input.GetButton("JoystickButton6"); // back
+        JoystickButton7 = Input.GetButton("JoystickButton7"); // start
+
+        JoystickButton8 = Input.GetButton("JoystickButton8"); // left thumbstick in
+        JoystickButton9 = Input.GetButton("JoystickButton9"); // right thumbstick in
+    }
+
+    private void DoXBOXMac()
+    {
+        JoystickAxis4 = Input.GetAxisRaw("JoystickAxis3"); // right thumbstick horizontal
+        JoystickAxis5 = Input.GetAxisRaw("JoystickAxis4"); // right thumbstick vertical
+
+        JoystickButton0 = Input.GetButton("JoystickButton16"); // bottom button
+        JoystickButton1 = Input.GetButton("JoystickButton17"); // right button
+        JoystickButton2 = Input.GetButton("JoystickButton18"); // left button
+        JoystickButton3 = Input.GetButton("JoystickButton19"); // top button
+
+        JoystickButton4 = Input.GetButton("JoystickButton13"); // left bumper
+        JoystickButton5 = Input.GetButton("JoystickButton14"); // right bumper
+        JoystickButton6 = Input.GetButton("JoystickButton10"); // back
+        JoystickButton7 = Input.GetButton("JoystickButton9"); // start
+
+        JoystickButton8 = Input.GetButton("JoystickButton11"); // left thumbstick in
+        JoystickButton9 = Input.GetButton("JoystickButton12"); // right thumbstick in
     }
 }
