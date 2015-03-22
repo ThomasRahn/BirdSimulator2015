85799e2048fc661a82abeab6ab7e04dafd01dca1
Quang Tran <tran.quang@live.com>
2015-03-21 21:54:55 -0400

    I think i fixed the collision problem?

--- BirdSimulator2015/Assets/Code/Scripts/Toolbox/IgnoreCollisions.cs
+++ BirdSimulator2015/Assets/Code/Scripts/Toolbox/IgnoreCollisions.cs
@@ -3,8 +3,20 @@ using System.Collections;
 
 public class IgnoreCollisions : MonoBehaviour 
 {
+	private Collider collider;
+
+	private void Awake()
+	{
+		this.collider = this.GetComponent<Collider>();
+	}
+
 	private void OnCollisionEnter(Collision c)
 	{
-		Physics.IgnoreCollision(c.collider, this.GetComponent<Collider>());
+		Physics.IgnoreCollision(c.collider, this.collider);
+	}
+
+	private void OnCollisionStay(Collision c)
+	{
+		Physics.IgnoreCollision(c.collider, this.collider);
 	}
 }
