3206a554be10caf322f57152a813a89added76c6
ThomasRahn <thomas@rahn.ca>
2015-03-22 19:40:43 -0400

    Removed default comments so david stops QQ'ing <3

--- BirdSimulator2015/Assets/Code/Scripts/Behaviour/CloudWall.cs
+++ BirdSimulator2015/Assets/Code/Scripts/Behaviour/CloudWall.cs
@@ -4,14 +4,6 @@ using System.Collections;
 public class CloudWall : MonoBehaviour 
 {
 	public int level = 1;
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
 
 	void OnCollisionEnter(Collision col)
 	{
