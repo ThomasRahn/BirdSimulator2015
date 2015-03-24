49aa5f42523703c328cee6eace27f9be21001b0b
David Siekut <davidsiekut@gmail.com>
2015-03-23 23:55:23 -0400

    MenuCamera works with any rotation.

--- BirdSimulator2015/Assets/Code/Scripts/Cam/MenuCamera.cs
+++ BirdSimulator2015/Assets/Code/Scripts/Cam/MenuCamera.cs
@@ -4,20 +4,22 @@ using System.Collections;
 public class MenuCamera : MonoBehaviour
 {
     private float rate = 0.2f;
+    private float origin;
 
 	void Start()
     {
+        origin = this.transform.localRotation.eulerAngles.y;
 	}
 	
 	void Update()
     {
         this.transform.Rotate(Vector3.up, rate * Time.deltaTime);
 
-        if (this.transform.localRotation.eulerAngles.y < 240)
+        if (this.transform.localRotation.eulerAngles.y < origin - 10)
         {
             rate *= -1;
         }
-        else if (this.transform.localRotation.eulerAngles.y > 260)
+        else if (this.transform.localRotation.eulerAngles.y > origin + 10)
         {
             rate *= -1;
         }
